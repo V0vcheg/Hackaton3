7fbb5a1aa7e377cc7ec8c679a516dca9ec51554a
V0vcheg <kremniov98@gmail.com>
2025-07-01 14:03:38 +0200

    prisma lib singleton

--- app/(auth)/files/page.tsx
+++ app/(auth)/files/page.tsx
@@ -1,4 +1,8 @@
-export default function FilesPage() {
+import prisma from "@/libs/prisma";
+
+export default async function FilesPage() {
+    const users = await prisma.user.findMany()
+    console.log(users);
     return (
         <>
             <h1>Files Page</h1>
